dec439d401890dbd6d1eaaaa430271d657ba7808
Ewy <EwyBoy@gmail.com>
2020-10-20 02:10:03 +0200

    Shop System

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+  public void PurchaseTower(GameObject tower)
+  {
+      
+  }
+
+
+}
